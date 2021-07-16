58f65f094cf3ef9298c833703deb537babe49eb7
abad89 <abadalamenti89@gmail.com>
2021-07-16 16:03:47 -0400

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+    
+    def learn(knowledge)
+        self.knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
